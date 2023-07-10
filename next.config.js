d23c3ef47a315cb3664348486ceb5566e6a122a1
xssxx <koonx6520@gmail.com>
2023-07-10 18:13:12 +0700

    change asset prefix to custom domain

--- next.config.js
+++ next.config.js
@@ -2,11 +2,10 @@
 require("dotenv").config();
 
 const isProd = process.env.NODE_ENV === "production";
-// console.log("isProd", process.env.NODE_ENV);
+console.log("isProd", process.env.NODE_ENV);
 
 const nextConfig = {
-  assetPrefix: isProd ? "/mutaru-tarot" : undefined,
-  basePath: isProd ? "/mutaru-tarot" : undefined,
+  assetPrefix: isProd ? "https://mutaru-tarot.com" : undefined,
   output: "export",
   reactStrictMode: true,
 };
