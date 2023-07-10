3bc5079c46fd016545014ba72119e6e443998202
xssxx <koonx6520@gmail.com>
2023-07-10 17:18:53 +0700

    add contact footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,8 +4,21 @@ import { SocialIcon } from "react-social-icons";
 
 const Footer = () => {
   return (
-    <div className="flex w-full bottom-0 py-2 border-2 bg-violet-200 justify-center">
-      <span>&copy; 2023 | Mutaru tarot</span>
+    <div className="flex w-full bottom-0 py-2 border-2 bg-violet-200 justify-center gap-4">
+      <span className="flex text-indigo-800 items-center">
+        &copy; 2023 | Mutaru tarot
+      </span>
+      <ul className="flex flex-row justify-center gap-4 items-center sm:gap-8">
+        <li className="flex items-center gap-2 cursor-pointer">
+          <SocialIcon network="facebook" style={{ height: 25, width: 25 }} />
+        </li>
+        <li className="flex items-center gap-2 cursor-pointer">
+          <SocialIcon network="instagram" style={{ height: 25, width: 25 }} />
+        </li>
+        <li className="flex items-center gap-2 cursor-pointer">
+          <Image src="line.svg" width={29} height={29} />
+        </li>
+      </ul>
     </div>
   );
 };
