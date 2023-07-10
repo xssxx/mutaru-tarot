4fcd47306ffa6b7dd316f18cf137c94556de02c2
xssxx <koonx6520@gmail.com>
2023-07-10 13:09:59 +0700

    add asset prefix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
+  assetPrefix: "/mutaru-tarot",
   output: "export",
   reactStrictMode: true,
   assetPrefix: isProd ? "/mutaru-tarot" : "",
