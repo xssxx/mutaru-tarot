8bf49fbd4caf1eb446f9470167b8e019646f608d
xssxx <koonx6520@gmail.com>
2023-07-10 12:53:18 +0700

    add export to next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-// module.exports = nextConfig
 
 const isProd = process.env.NODE_ENV === "production";
 
-module.exports = {
+const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   assetPrefix: isProd ? "/mutaru-tarot" : "",
 };
+
+module.exports = nextConfig;
