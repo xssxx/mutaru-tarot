e6448ae0befd094048628ecb16930dc507483025
xssxx <koonx6520@gmail.com>
2023-07-10 14:10:11 +0700

    fix asset prefix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
-  assetPrefix: isProd ? "mutaru-tarot" : "",
+  assetPrefix: isProd ? "/mutaru-tarot" : "",
 };
 
 module.exports = nextConfig;
